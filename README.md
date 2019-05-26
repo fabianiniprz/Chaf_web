937275ede4fb26fd91205b3497a1f2088e009929
fabianiniprz <fabianiniprz@gmail.com>
2019-05-26 16:23:21 -0500

    log_principal

---
+++ README.md
@@ -0,0 +1,1 @@
+CHAF_WEB
---
+++ app.js
@@ -0,0 +1,11 @@
+function funcion1(){
+document.write("<h1>Hola fabian</h1>")
+};
+
+function funcion2(){
+	document.getElementById("username").innerHTML("")
+};
+
+function funcion3(){
+	document.getElementById("password").innerHTML("")
+};
